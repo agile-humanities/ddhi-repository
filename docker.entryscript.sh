ccd9fd770a4685f37f44c953aa28349624334c97
Bill Kennedy <bill@agilehumanities.ca>
2020-04-16 16:43:48 -0400

    Tweaking Entry Script
    Signed-off-by: Bill Kennedy <bill@agilehumanities.ca>

--- docker.entryscript.sh
+++ docker.entryscript.sh
@@ -4,18 +4,22 @@ INSTALLLOCKFILE='/repository/docroot/sites/default/files/install.lock'
 
 if [ ! -f $INSTALLLOCKFILE ]
 then
-    drupal site:install  standard  \
+    cp ./docroot/sites/default/default.settings.php ./docroot/sites/default/settings.php
+    chown www-data:www-data ./docroot/sites/default/settings.php
+    drupal site:install -n standard  \
       --langcode=$DRUPAL_LANGCODE  \
       --db-type=$DRUPAL_DB_TYPE  \
       --db-host=$DRUPAL_DATABASE_HOST  \
       --db-name=$DRUPAL_DATABASE_NAME \
-      --db-user=$DRUPAL_DATABASE_USERNAME  \
-      --db-pass=$DRUPAL_DATABASE_PASSWORD  \
-      --db-port=$DRUPAL_DATABASE_PORT  \
-      --site-name=$DRUPAL_SITE_NAME  \
-      --site-mail=$DRUPAL_SITE_MAIL  \
-      --account-name=$DRUPAL_SITE_ADMIN_ACCOUNT_NAME  \
-      --account-mail=$DRUPAL_SITE_ADMIN_ACCOUNT_MAIL  \
+      --db-user=$DRUPAL_DATABASE_USERNAME \
+      --db-pass=$DRUPAL_DATABASE_PASSWORD \
+      --db-port=$DRUPAL_DATABASE_PORT \
+      --site-name="$DRUPAL_SITE_NAME" \
+      --site-mail=$DRUPAL_SITE_MAIL \
+      --account-name="$DRUPAL_SITE_ADMIN_ACCOUNT_NAME" \
+      --account-mail=$DRUPAL_SITE_ADMIN_ACCOUNT_MAIL \
       --account-pass=$DRUPAL_SITE_ADMIN_ACCOUNT_PASSWORD
     touch $INSTALLLOCKFILE
+elif [ $DRUPAL_UPDATE = "yes" ]
+    composer update
 fi
