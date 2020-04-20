0042f1d7e5598862d89f5996f74b642629964b3a
Bill Kennedy <bill@agilehumanities.ca>
2020-04-20 17:20:46 -0400

    Configuration Changes
    - Running configuration through Drupal config
    - Moved Drupal install prep from DockerFile into entry script
    - Added supporting libraries to composer config
    
    Signed-off-by: Bill Kennedy <bill@agilehumanities.ca>

--- scripts/docker.entryscript.sh
+++ scripts/docker.entryscript.sh
@@ -4,10 +4,22 @@ INSTALLLOCKFILE='/repository/docroot/sites/default/files/install.lock'
 
 if [ ! -f $INSTALLLOCKFILE ]
 then
+    # Prep Git
+    git config --global user.email $GIT_ADMIN_MAIL
+    git config --global user.name \"$GIT USER\"
+
+    # Prep the Drupal install process
     echo "Drupal instance not found. Beginning installation."
-    # Prep the install process
     cp ./docroot/sites/default/default.settings.php ./docroot/sites/default/settings.php
     chown www-data:www-data ./docroot/sites/default/settings.php
+    mkdir docroot/sites/default/files;
+    chown www-data:www-data docroot/sites/default/files;
+    chmod 755 docroot/sites/default/files;
+    chown -R www-data:www-data ./config
+
+    # Prep settings file for local settings
+    echo "if (file_exists(\$app_root . '/' . \$site_path . '/settings.container.php')) { include \$app_root . '/' . \$site_path . '/settings.local.php'; }" >> /repository/docroot/sites/default/settings.php
+
     # Run Drupal console Install
     drupal site:install -n standard  \
       --langcode=$DRUPAL_LANGCODE  \
@@ -22,24 +34,29 @@ then
       --account-name="$DRUPAL_SITE_ADMIN_ACCOUNT_NAME" \
       --account-mail=$DRUPAL_SITE_ADMIN_ACCOUNT_MAIL \
       --account-pass=$DRUPAL_SITE_ADMIN_ACCOUNT_PASSWORD
+
+    # Run configuration suite
+    drupal config:import --directory=/repository/preconfig
+
     # Use Drush to install module suite. @todo: may require a more sophisticated profile
     # system here
-    drush pm-enable \
-            ctools \
-            token \
-            devel \
-            pathauto \
-            auto_entitylabel \
-            rabbit_hole \
-            svg_image \
-            migrate_tools \
-            migrate_source_csv \
-            migrate_plus \
-            paragraphs
+    # drush pm-enable \
+    #        ctools \
+    #        token \
+    #        devel \
+    #        pathauto \
+    #        auto_entitylabel \
+    #        rabbit_hole \
+    #        svg_image \
+    #        migrate_tools \
+    #        migrate_source_csv \
+    #        migrate_plus \
+    #        paragraphs
     # Install a lockfile in persistent storage to indicate installation
     # @todo: This should run a test (perhaps on drupal console output) to
     # ensure installation was successful before writing
     touch $INSTALLLOCKFILE
 else
     echo "Drupal instance found. No installation required."
+    # php -d memory_limit=-1 /usr/local/bin/composer update
 fi
