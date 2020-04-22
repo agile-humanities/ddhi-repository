ebfbc9ace121bc1bf0848c44109ffd72d69944a1
Bill Kennedy <bill@agilehumanities.ca>
2020-04-22 18:18:12 -0400

    Solidified entity extraction method
    - Previous build’s methodology was sketchy and fragile
    - Xpath logic put in plugin for more control
    
    Signed-off-by: Bill Kennedy <bill@agilehumanities.ca>

--- docroot/modules/custom/ddhi_ingest/src/Plugin/migrate/process/ExtractNamedEntities.php
+++ docroot/modules/custom/ddhi_ingest/src/Plugin/migrate/process/ExtractNamedEntities.php
@@ -20,20 +20,40 @@ class ExtractNamedEntities extends ProcessPluginBase {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     
-    $output = '';
-    
-    if (!empty($value)) {
-      
-      $values = array();
-      foreach($value as $item) {
-        $values[] = $item;
-      }
-      $values = array_unique($values);
-      asort($values);
-      $output = join("\r",$values);
+    if (empty($value) || !is_object($value)) {
+      return '';
     }
     
+   
+    $value->registerXPathNamespace("tei","http://www.tei-c.org/ns/1.0");
+    $items = $value->xpath('//tei:placeName');
+    $values = [];
+    
+        
+    if ($items !== false) {
+    
+      foreach ($items as $item) {
+        $text = $item->__toString();
+        $values[strtolower($text)] = $text; // Creates unique values and natural sorting order
+      }
+      
+      ksort($values);
+          
+      return join("\r",array_values($values));
+    
+    } 
+    
+         /*   
+    foreach((array)$value as $item) {
+      $values[] = $item;
+    }
+    
+    $values = array_unique($values);
+    asort($values);
+    $output = join("\r",$values);
+    
     return $output;
+    */
   }
 
 }
