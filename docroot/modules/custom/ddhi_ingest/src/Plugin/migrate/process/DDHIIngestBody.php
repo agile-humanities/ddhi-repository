b7313d27a9731de316ffec540c5edf94e32c1d4d
Bill Kennedy <bill@agilehumanities.ca>
2021-01-12 17:07:39 -0500

    Fixed body text import issue
    - Body text was coming in blank.
    - Now passes body text through original migration plugin, which converts the value to XHTML.

--- docroot/modules/custom/ddhi_ingest/src/Plugin/migrate/process/DDHIIngestBody.php
+++ docroot/modules/custom/ddhi_ingest/src/Plugin/migrate/process/DDHIIngestBody.php
@@ -19,21 +19,22 @@ class DDHIIngestBody extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    
-    
+    $messenger = \Drupal::messenger();
+    dpm($value);
+
     if (is_object($value)) {
       // Strip paragraphs
-      
+
       $output = $value->asXML();
       $output = preg_replace('~[[:cntrl:]]~', '',$output);
-      
+
       $utterance_pattern = '/<u who=\"([^\"]*)\">(.*?)<\/u>/i';
       $utterance_replacement = "<h3>$1:</h3><p>$2</p>";
       return preg_replace($utterance_pattern,$utterance_replacement,$output);
     } else if (is_string($value)) {
       return $value;
     }
-    
+
     return '';
   }
 
